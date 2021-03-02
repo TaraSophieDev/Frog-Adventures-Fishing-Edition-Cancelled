d1ff962c80233c78b40348cf4a9f556e518269a5
TerraDev64 <TerraDev64@gmail.com>
2021-03-02 18:26:37 +0100

    initial commit

---
+++ Player/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+onready var sprite = $Sprite
+
+var speed = 20
+
+var motion = Vector2()
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("right"):
+		motion.x = speed
+		sprite.flip_h = false
+	elif Input.is_action_pressed("left"):
+		motion.x = -speed
+		sprite.flip_h = true
+	else:
+		motion.x = 0
+		
+	motion = move_and_slide(motion)
