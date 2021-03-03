1da54ae0fe39d293a44a6aaf3e4ee7cb8b582588
TerraDev64 <TerraDev64@gmail.com>
2021-03-03 22:39:05 +0100

    changed sprite orientation and player speed

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 onready var sprite = $Sprite
 
-var speed = 20
+var speed = 25
 
 var motion = Vector2()
 
@@ -13,10 +13,10 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
 		motion.x = speed
-		sprite.flip_h = false
+		sprite.flip_h = true
 	elif Input.is_action_pressed("left"):
 		motion.x = -speed
-		sprite.flip_h = true
+		sprite.flip_h = false
 	else:
 		motion.x = 0
 		
