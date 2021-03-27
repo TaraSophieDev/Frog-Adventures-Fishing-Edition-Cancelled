550dce9f2c60b86c09587b74e72bc9b005c691b6
TerraDev64 <TerraDev64@gmail.com>
2021-03-27 18:14:14 +0100

    -

--- Misc/State.gd
+++ Misc/State.gd
@@ -9,16 +9,16 @@ var velocity = 0
 
 
 func _process(_delta):
-    persistent_state.move_and_slide(persistent_state.velocity)
+	persistent_state.move_and_slide(persistent_state.velocity)
 
 func setup(change_state, animated_sprite, persistent_state):
-    self.change_state = change_state
-    self.animated_sprite = animated_sprite
-    self.persistent_state = persistent_state
+	self.change_state = change_state
+	self.animated_sprite = animated_sprite
+	self.persistent_state = persistent_state
 
 func move_left():
-    pass
+	pass
 
 func move_right():
-    pass
+	pass
 
