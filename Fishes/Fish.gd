c0b296e182ae5d0aa060c6b8e10a31b047e204ce
TerraDev64 <TerraDev64@gmail.com>
2021-03-31 00:04:21 +0200

    Fish AI without baited state

--- Fishes/Fish.gd
+++ Fishes/Fish.gd
@@ -1,51 +1,44 @@
-#extends KinematicBody2D
-#
-#var swim_speed = 10
-#var baited_speed = 12
-#var time = 0
-#
-#onready var timer = $Timer
-#
-#var motion = Vector2()
-#
-#
-#enum state {
-#	swimming,
-#	idle,
-#	baited
-#}
-#
-#var fish_state = state.swimming
-##var fish_state = state.swimming
-#
-#func process_state():
-#	match fish_state:
-#		state.idle:
-#			#print("Idle")
-#			process_idle()
-#		state.swimming:
-#			#print("Swimming")
-#			process_swimming()
-#		state.baited:
-#			#print("Baited")
-#			process_baited()
-#
-#func process_baited():
-#	pass
-#
-#func process_swimming():
-#	motion.x = swim_speed
-#
-#func process_idle():
-#	pass
-#
-#func timer_rand():
-#	timer.start(rand_range(2,6))
-#	print(timer.wait_time)
-#
-#func _ready():
-#	timer_rand()
-#
-#func _process(delta):
-#	process_state()
-#	motion = move_and_slide(motion)
+extends KinematicBody2D
+
+onready var sprite = $Sprite
+
+enum {
+	IDLE,
+	NEW_DIR,
+	MOVE
+}
+
+func _ready():
+	randomize()
+
+var SPEED = 5
+var state = IDLE
+var dir = Vector2.RIGHT
+
+func _process(delta):
+	match state:
+		IDLE:
+			pass
+		NEW_DIR:
+			dir = choose([Vector2.RIGHT, Vector2.LEFT])
+			state = choose([IDLE, MOVE])
+		MOVE:
+			move(delta)
+
+func move(delta):
+	position += dir * SPEED * delta
+	print(dir.x)
+	if dir.x == 1:
+		sprite.flip_h = true
+	elif dir.x == -1:
+		sprite.flip_h = false
+
+func choose(array):
+	array.shuffle()
+	return array.front()
+
+
+func _on_Timer_timeout():
+	$Timer.wait_time = choose([0.25, 0.5, 1])
+	print($Timer.wait_time)
+	state = choose([IDLE, NEW_DIR, MOVE])
