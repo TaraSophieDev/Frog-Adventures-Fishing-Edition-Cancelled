568daee87a0a97c6a6313c9ef36fa1d671dfbe53
TerraDev64 <TerraDev64@gmail.com>
2021-04-01 23:31:48 +0200

    fixed bait flip_h bug

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,22 +1,30 @@
 extends KinematicBody2D
 
-onready var sprite = $Sprite
+onready var frogSprite = $FrogSprite
+onready var baitSprite = $FrogSprite/Bait/Sprite
+onready var ap = $AnimationPlayer
+onready var sp = $SwimmingPlayer
 
 var speed = 25
 
 var motion = Vector2()
 
 func _ready():
+	sp.play("swimming")
 	pass
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
 		motion.x = speed
-		sprite.flip_h = true
+		frogSprite.flip_h = true
+		baitSprite.flip_h = true
+		ap.play("bait_right")
 	elif Input.is_action_pressed("left"):
 		motion.x = -speed
-		sprite.flip_h = false
+		frogSprite.flip_h = false
+		baitSprite.flip_h = false
+		ap.play("bait_left")
 	else:
 		motion.x = 0
 		
