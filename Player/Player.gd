38b81c037f55096253a755c8a311e1fc937c29f6
TerraDev64 <TerraDev64@gmail.com>
2021-04-05 22:54:01 +0200

    WIP fishing gameplay

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,30 +2,52 @@ extends KinematicBody2D
 
 onready var frogSprite = $FrogSprite
 onready var baitSprite = $FrogSprite/Bait/Sprite
-onready var ap = $AnimationPlayer
-onready var sp = $SwimmingPlayer
+onready var swimP = $SwimmingPlayer
+onready var swingP = $SwingPlayer
 
+
+enum {
+	MOVING,
+	THROWING,
+	FISHING,
+	CATCHING
+}
 var speed = 25
 
 var motion = Vector2()
+var state = MOVING
 
 func _ready():
-	sp.play("swimming")
+	swimP.play("swimming")
 	pass
 
-func _physics_process(delta):
+func _process(delta):
+	match state:
+		MOVING:
+			move()
+		THROWING:
+			playThrowingAnim()
+		FISHING:
+			print("Fishing")
+		CATCHING:
+			pass
 	
+	#Checks if a button is activated		
+	if Input.is_action_just_pressed("a_button"):
+		state = THROWING
+	
+	motion = move_and_slide(motion)
+
+func move():
 	if Input.is_action_pressed("right"):
 		motion.x = speed
 		frogSprite.flip_h = true
-		baitSprite.flip_h = true
-		ap.play("bait_right")
 	elif Input.is_action_pressed("left"):
 		motion.x = -speed
 		frogSprite.flip_h = false
-		baitSprite.flip_h = false
-		ap.play("bait_left")
 	else:
 		motion.x = 0
-		
-	motion = move_and_slide(motion)
+
+func playThrowingAnim():
+	swingP.play("swing")
+	state = FISHING
