50a40f2a0bb4d155418636ed6cfdf63df0d556c5
TerraDev64 <TerraDev64@gmail.com>
2021-04-14 17:09:22 +0200

    renamed _on_bait_activation to activate_bait

--- Fishes/Fish.gd
+++ Fishes/Fish.gd
@@ -7,55 +7,55 @@ onready var rc = $RC
 signal baited
 
 enum {
-    IDLE,
-    NEW_DIR,
-    MOVE,
-    BAITED
+	IDLE,
+	NEW_DIR,
+	MOVE,
+	BAITED
 }
 
 func _ready():
-    randomize()
+	randomize()
 
 var SPEED = 10
 var state = IDLE
 var dir = Vector2.RIGHT
 
 func _process(delta):
-    if rc.is_colliding():
-        state = baited()
-    match state:
-        IDLE:
-            ap.stop()
-        NEW_DIR:
-            dir = choose([Vector2.RIGHT, Vector2.LEFT])
-            state = choose([IDLE, MOVE])
-        MOVE:
-            move(delta)
-        BAITED:
-            baited()
+	if rc.is_colliding():
+		state = baited()
+	match state:
+		IDLE:
+			ap.stop()
+		NEW_DIR:
+			dir = choose([Vector2.RIGHT, Vector2.LEFT])
+			state = choose([IDLE, MOVE])
+		MOVE:
+			move(delta)
+		BAITED:
+			baited()
 
 func move(delta):
-    # Rotates raycast
-    var temp = rad2deg(atan2(-dir.y, -dir.x))
-    rc.rotation_degrees = temp
+	# Rotates raycast
+	var temp = rad2deg(atan2(-dir.y, -dir.x))
+	rc.rotation_degrees = temp
 
-    ap.play("Swim")
-    position += dir * SPEED * delta
-    if dir.x == 1:
-        sprite.flip_h = true
-    elif dir.x == -1:
-        sprite.flip_h = false
+	ap.play("Swim")
+	position += dir * SPEED * delta
+	if dir.x == 1:
+		sprite.flip_h = true
+	elif dir.x == -1:
+		sprite.flip_h = false
 
 func choose(array):
-    array.shuffle()
-    return array.front()
+	array.shuffle()
+	return array.front()
 
 func baited():
-    ap.play("Chase")
-    emit_signal("baited")
-    print("baited")
+	ap.play("Chase")
+	emit_signal("baited")
+	print("baited")
 
 func _on_Timer_timeout():
-    $Timer.wait_time = choose([1, 1.5, 2])
-    print("Timer wait time: ", $Timer.wait_time)
-    state = choose([IDLE, NEW_DIR, MOVE])
+	$Timer.wait_time = choose([1, 1.5, 2])
+	print("Timer wait time: ", $Timer.wait_time)
+	state = choose([IDLE, NEW_DIR, MOVE])
--- Misc/BaitEntity.gd
+++ Misc/BaitEntity.gd
@@ -13,68 +13,68 @@ var jump_force = 80
 
 
 enum {
-    IDLE,
-    MOVING,
-    BAITED,
-    CATCHING
+	IDLE,
+	MOVING,
+	BAITED,
+	CATCHING
 }
 
 var state = IDLE
 
 func _physics_process(delta):
-    match state:
-        IDLE:
-            sleeping = true
-        MOVING:
-            #$Camera2D.current = true
-            sleeping = false
-            move(delta)
-            #motion.y += gravity
-        BAITED:
-            baited()
-        CATCHING:
-            catching()
-        
+	match state:
+		IDLE:
+			sleeping = true
+		MOVING:
+			#$Camera2D.current = true
+			sleeping = false
+			move(delta)
+			#motion.y += gravity
+		BAITED:
+			baited()
+		CATCHING:
+			catching()
+		
 
-    #motion = move_and_slide(motion)
+	#motion = move_and_slide(motion)
 
 func move(delta):
-    if Input.is_action_just_pressed("a_button"):
-        #check if x coords is smaller than the frog x coords and otherwise
-        print("player pos: ", player.position.x, " Bait Pos: ", global_position.x)
-        if global_position.x > player.position.x - 1:
-            apply_impulse(Vector2(0,0), Vector2(-speed, 0))
-            $Sprite.flip_h = true
-        if global_position.x < player.position.x + 1:
-            apply_impulse(Vector2(0,0), Vector2(speed, 0))
-            $Sprite.flip_h = false
-        apply_central_impulse(Vector2.UP * jump_force)
+	if Input.is_action_just_pressed("a_button"):
+		#check if x coords is smaller than the frog x coords and otherwise
+		print("player pos: ", player.position.x, " Bait Pos: ", global_position.x)
+		if global_position.x > player.position.x - 1:
+			apply_impulse(Vector2(0,0), Vector2(-speed, 0))
+			$Sprite.flip_h = true
+		if global_position.x < player.position.x + 1:
+			apply_impulse(Vector2(0,0), Vector2(speed, 0))
+			$Sprite.flip_h = false
+		apply_central_impulse(Vector2.UP * jump_force)
 
-    #sets sprite to y velocity
-    if get_linear_velocity().y < 0.1:
-        $Sprite.frame = 0
-    elif get_linear_velocity().y > 0.1:
-        $Sprite.frame = 1
-    elif get_linear_velocity().y == 0:
-        $Sprite.frame = 1
+	#sets sprite to y velocity
+	if get_linear_velocity().y < 0.1:
+		$Sprite.frame = 0
+	elif get_linear_velocity().y > 0.1:
+		$Sprite.frame = 1
+	elif get_linear_velocity().y == 0:
+		$Sprite.frame = 1
 
 
 
 
 func baited():
-    pass
+	pass
 
 func catching():
-    pass
+	pass
 
 
 
 func _ready():
-    pass
+	pass
 
 
-func _on_bait_activation():
-    yield(get_tree().create_timer(1.05), "timeout")
-    $CollisionShape2D.disabled = false
-    state = MOVING
+func activate_bait():
+	yield(get_tree().create_timer(1.05), "timeout")
+	$CollisionShape2D.disabled = false
+	state = MOVING
 
