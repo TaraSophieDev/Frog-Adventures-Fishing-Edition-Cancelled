fe100db0b0f937cc553c165793fb3bd5e9fc13c0
Ian Williams <ian@iwillia.ms>
2021-04-13 18:01:51 -0700

    Feedback changes

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,17 +9,17 @@ onready var baitSpawn = $BaitSpawn
 #onready var baitSpawnRight = $BaitSpawnRight
 
 #onready var bait = get_node("BaitEntity")
-onready var bait_instance = preload("res://Misc/BaitEntity.tscn")
-#var bait_instance = bait.instance()
+onready var bait_scene = preload("res://Misc/BaitEntity.tscn")
+var bait_instance = null
 
 signal bait_activation
 
 
 enum {
-	MOVING,
-	THROWING,
-	FISHING,
-	CATCHING
+    MOVING,
+    THROWING,
+    FISHING,
+    CATCHING
 }
 var speed = 25
 
@@ -28,84 +28,86 @@ var state = MOVING
 
 
 func _ready():
-	swimP.play("swimming")
-	pass
+    swimP.play("swimming")
+    pass
 
 func _process(delta):
-	match state:
-		MOVING:
-			move()
-			#bait.sleeping = true
-		THROWING:
-			playThrowingAnim(delta)
-		FISHING:
-			emit_signal("bait_activation")
-		CATCHING:
-			pass		
-	
-	if frogSprite.flip_h == true:
-		baitSpawn.position = Vector2(20, -5)
-	else:
-		baitSpawn.position = Vector2(-20, -5)
-		
-	motion = move_and_slide(motion)
+    match state:
+        MOVING:
+            move()
+        THROWING:
+            pass
+        FISHING:
+            pass
+        CATCHING:
+            pass
+    
+    if frogSprite.flip_h == true:
+        baitSpawn.position = Vector2(25, -5)
+    else:
+        baitSpawn.position = Vector2(-20, -5)
+        
+    motion = move_and_slide(motion)
+    
+    
+func _physics_process(delta):
+    if Input.is_action_just_pressed("a_button") and state == MOVING:
+        playThrowingAnim(delta)        
+        motion.x = 0
+        state = THROWING
+    
 
 func move():
-	#bait.visible = false
-	frogSprite.frame = 0
-	#Movement
-	if Input.is_action_pressed("right"):
-		motion.x = speed
-		frogSprite.flip_h = true
-	elif Input.is_action_pressed("left"):
-		motion.x = -speed
-		frogSprite.flip_h = false
-	else:
-		motion.x = 0
-
-	if Input.is_action_just_pressed("a_button"):
-		#sets bait position
-		if frogSprite.flip_h == true:
-			bait_instance.position = baitSpawn.position
-		elif frogSprite.flip_h == false:
-			bait_instance.position = baitSpawn.position
-		
-		motion.x = 0
-		state = THROWING
-	
-		
+    #bait.visible = false
+    frogSprite.frame = 0
+    #Movement
+    if Input.is_action_pressed("right"):
+        motion.x = speed
+        frogSprite.flip_h = true
+    elif Input.is_action_pressed("left"):
+        motion.x = -speed
+        frogSprite.flip_h = false
+    else:
+        motion.x = 0
 
 
 func playThrowingAnim(delta):
-	swingP.play("swing")
-	spawnBait(delta)
-	state = FISHING
-	
+    state = THROWING
+    swingP.play("swing")
+    spawnBait(delta)
+
+    
 func spawnBait(delta):
-	bait_instance = null
-	add_child(bait_instance)
-	#var bait_instance = bait.instance()
-	yield(get_tree().create_timer(1.10), "timeout")
-	bait_instance.show()
-	bait_instance.sleeping = true
-	if frogSprite.flip_h == false:
-		bait_instance.get_node("Sprite").flip_h = true
-		#bait.global_position = baitSpawnLeft.get_global_position()
-		bait_instance.sleeping = false
-		bait_instance.apply_impulse(Vector2(0, 0), Vector2(-100, 0))
-		state = FISHING
-	if frogSprite.flip_h == true:
-		bait_instance.get_node("Sprite").flip_h = false
-		#bait.global_position = baitSpawnRight.get_global_position()
-		bait_instance.sleeping = false
-		bait_instance.apply_impulse(Vector2(0, 0), Vector2(100, 0))
-		state = FISHING
-	#get_tree().get_root().add_child(bait_instance)
+    if is_instance_valid(bait_instance):
+        bait_instance.queue_free()
+    bait_instance = bait_scene.instance()
+    if frogSprite.flip_h == true:
+        bait_instance.position = baitSpawn.position
+    elif frogSprite.flip_h == false:
+        bait_instance.position = baitSpawn.position
+    add_child(bait_instance)
+    yield(get_tree().create_timer(1.10), "timeout")
+    bait_instance.show()
+    bait_instance.sleeping = true
+    if frogSprite.flip_h == false:
+        bait_instance.get_node("Sprite").flip_h = true
+        #bait.global_position = baitSpawnLeft.get_global_position()
+        bait_instance.sleeping = false
+        bait_instance.apply_impulse(Vector2(0, 0), Vector2(-100, 0))
+        state = FISHING
+    if frogSprite.flip_h == true:
+        bait_instance.get_node("Sprite").flip_h = false
+        #bait.global_position = baitSpawnRight.get_global_position()
+        bait_instance.sleeping = false
+        bait_instance.apply_impulse(Vector2(0, 0), Vector2(100, 0))
+        state = FISHING
+    bait_instance._on_bait_activation()
+    #get_tree().get_root().add_child(bait_instance)
 
 
 
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("bait"):
-		bait_instance.queue_free()
-		state = MOVING
+    if body.is_in_group("bait") and is_instance_valid(bait_instance) and state == FISHING:
+        bait_instance.queue_free()
+        state = MOVING
